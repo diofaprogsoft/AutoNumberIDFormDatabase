9b5012f137482d5e03fd48d8c654f0d4c20272a9
diofaprogsoft <diofanta@gmail.com>
2019-09-28 16:38:20 +0700

    Add files via upload

---
+++ AutoNumber/FormAutoNumber.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormAutoNumber
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.IdTextBox = New System.Windows.Forms.TextBox()
+        Me.AddButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.IdDataGridView = New System.Windows.Forms.DataGridView()
+        CType(Me.IdDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'IdTextBox
+        '
+        Me.IdTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.IdTextBox.Name = "IdTextBox"
+        Me.IdTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.IdTextBox.TabIndex = 0
+        '
+        'AddButton
+        '
+        Me.AddButton.Location = New System.Drawing.Point(12, 59)
+        Me.AddButton.Name = "AddButton"
+        Me.AddButton.Size = New System.Drawing.Size(100, 23)
+        Me.AddButton.TabIndex = 1
+        Me.AddButton.Text = "Add"
+        Me.AddButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(12, 88)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(100, 23)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'IdDataGridView
+        '
+        Me.IdDataGridView.AllowUserToAddRows = False
+        Me.IdDataGridView.AllowUserToDeleteRows = False
+        Me.IdDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.IdDataGridView.Location = New System.Drawing.Point(118, 12)
+        Me.IdDataGridView.MultiSelect = False
+        Me.IdDataGridView.Name = "IdDataGridView"
+        Me.IdDataGridView.ReadOnly = True
+        Me.IdDataGridView.Size = New System.Drawing.Size(213, 99)
+        Me.IdDataGridView.TabIndex = 3
+        '
+        'FormAutoNumber
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(343, 123)
+        Me.Controls.Add(Me.IdDataGridView)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.AddButton)
+        Me.Controls.Add(Me.IdTextBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormAutoNumber"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AutoNumber"
+        CType(Me.IdDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents IdTextBox As TextBox
+    Friend WithEvents AddButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents IdDataGridView As DataGridView
+End Class
---
+++ AutoNumber/FormAutoNumber.vb
@@ -0,0 +1,57 @@
+﻿Public Class FormAutoNumber
+    Private ReadOnly Property Con As SqlClient.SqlConnection = New SqlClient.SqlConnection(String.Format("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={0};Integrated Security=True", IO.Path.Combine(Application.StartupPath, "SampleDatabase.mdf")))
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call CheckPID()
+    End Sub
+
+    Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
+        Using command As New SqlClient.SqlCommand
+            With command
+                .Connection = Con
+                .CommandType = CommandType.Text
+                .CommandText = "INSERT INTO TableID(PID) VALUES(@pid)"
+                .Parameters.AddWithValue("@pid", Me.IdTextBox.Text)
+            End With
+            Con.Open()
+            command.ExecuteNonQuery()
+        End Using
+        Con.Close()
+
+        Call CheckPID()
+        Dim i = Me.IdDataGridView.RowCount - 1
+        Me.IdDataGridView.FirstDisplayedScrollingRowIndex = i
+        Me.IdDataGridView.Rows(i).Selected = True
+        Me.IdDataGridView.CurrentCell = Me.IdDataGridView.Rows(i).Cells(0)
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        Using command As New SqlClient.SqlCommand("DELETE FROM TableID", Con)
+            Con.Open()
+            command.ExecuteNonQuery()
+        End Using
+        Con.Close()
+
+        Call CheckPID()
+    End Sub
+
+    Private Sub CheckPID()
+        Me.IdDataGridView.DataSource = Nothing
+        Using command As New SqlClient.SqlCommand("select PID from TableID", Con)
+            Dim adapter As New SqlClient.SqlDataAdapter(command)
+            Dim dt As New DataTable
+            adapter.Fill(dt)
+            Con.Open()
+
+            Dim dr = command.ExecuteReader()
+            If Not dr.HasRows Then
+                Me.IdTextBox.Text = "P0001"
+            Else
+                Me.IdTextBox.Text = String.Format("P{0:0000}", dt.Rows.Count + 1)
+            End If
+
+            Me.IdDataGridView.DataSource = dt
+        End Using
+        Con.Close()
+    End Sub
+End Class
